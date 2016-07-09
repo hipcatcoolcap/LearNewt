501b57907c8c7b245871067b72f8324c40bd5b79
hipcatcoolcap <hipcatcoolcap@gmail.com>
2016-07-09 17:37:58 -0500

    added a time/date stamp in the upper left corner tied to the string in the bottom.

--- src/main.cpp
+++ src/main.cpp
@@ -3,34 +3,45 @@
 #include <unistd.h>
 #include <iostream>
 #include <stdio.h>
-//#include <MAIN.H>
+#include <time.h>
 
+
+void print_time();
 
 void print_lower_right(std::string test)
 {
 	char TEXT [120];
 	sprintf(TEXT,"%s",test.c_str());
 	newtDrawRootText(-1*test.size(), -2, TEXT);
+	print_time();
 	newtRefresh();
 	newtWaitForKey();
 }
 
+void print_time()
+
+{
+	clock_t t;
+	t = clock();
+	time(&t);
+	char TEXT [120];
+	sprintf(TEXT,"%s",asctime(localtime(&t)));
+	newtDrawRootText(-20, 1, TEXT);
+	newtRefresh();
+	newtWaitForKey();
+}
 int main(void) {
 
 	newtInit();
 	newtCls();
 	newtDrawRootText(0, 0, "Temprature Coefficient Test System");
 	print_lower_right("TEST STRING");
-
 	newtPushHelpLine("I AM NO HELP AT ALL");
 	newtRefresh();
 	newtWaitForKey();
-
 	print_lower_right("second string");
-
 	newtPopHelpLine();
 	newtRefresh();
 	sleep(1);
-
 	newtFinished();
 }
