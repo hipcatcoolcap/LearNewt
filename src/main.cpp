22053454a0d80f708b800fea50d00455dd5bbddf
hipcatcoolcap <hipcatcoolcap@gmail.com>
2016-07-10 14:47:57 -0500

    added a spot for the temprature, as well as a header for the devices to be listed and tracked by the GUI.

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,42 @@
 #include <iostream>
 #include <stdio.h>
 #include <time.h>
+#include <vector>
 
-
+//function list
+int main (void);
 void print_time();
+void print_lower_right();
+void print_screen(); 
+void print_table_header();
 
-void print_lower_right(std::string test)
+std::string lower_right;
+std::string HelpTest;
+bool running;
+std::vector <float> Temprature;
+
+void print_table_header()
 {
 	char TEXT [120];
-	sprintf(TEXT,"%s",test.c_str());
-	newtDrawRootText(-1*test.size(), -2, TEXT);
+	sprintf(TEXT,"position number\t previous reading\t current reading\t delta\t std");
+	newtDrawRootText(1, 2, TEXT);
+
+}
+void print_screen()
+{
+ 	char help [120];
+	sprintf(help,"%s",HelpTest.c_str());
+	newtPushHelpLine(help);
 	print_time();
+	print_table_header();
+	print_lower_right();
+	newtRefresh();
+}
+void print_lower_right()
+{
+	char TEXT [120];
+	sprintf(TEXT,"%s",lower_right.c_str());
+	newtDrawRootText(-1*lower_right.size(), -2, TEXT);
 	newtRefresh();
 	newtWaitForKey();
 }
@@ -25,23 +51,27 @@ void print_time()
 	t = clock();
 	time(&t);
 	char TEXT [120];
+	sprintf(TEXT,"Temprature is %f",Temprature.back());
+	newtDrawRootText(1, 1, TEXT);
+
 	sprintf(TEXT,"%s",asctime(localtime(&t)));
 	newtDrawRootText(-20, 1, TEXT);
 	newtRefresh();
 	newtWaitForKey();
 }
 int main(void) {
-
+	Temprature.push_back(999999);
 	newtInit();
 	newtCls();
 	newtDrawRootText(0, 0, "Temprature Coefficient Test System");
-	print_lower_right("TEST STRING");
-	newtPushHelpLine("I AM NO HELP AT ALL");
-	newtRefresh();
+	lower_right= "TEST STRING";
+	HelpTest = "I AM NO HELP AT ALL";
+	print_screen();
 	newtWaitForKey();
-	print_lower_right("second string");
-	newtPopHelpLine();
-	newtRefresh();
+	lower_right = "second string";
+	//newtPopHelpLine();
+	
+	print_screen();
 	sleep(1);
 	newtFinished();
 }
